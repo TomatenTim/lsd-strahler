028fb71f80fc5f5ddbc0af97017761829048927c
TomatenTim <77119458+TomatenTim@users.noreply.github.com>
2024-06-09 18:14:02 +0200

    create pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
+import withPWA from 'next-pwa';
+
+const pwa = withPWA({
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+export default pwa(nextConfig);
